1138197469fd9585876076c2f4f40b1f9c914301
MinKi1506 <93629619+MinKi1506@users.noreply.github.com>
2022-08-04 18:30:40 +0900

    💔리뷰 추가 페이지(10)

--- src/pages/PlaceDetail/AddReview/AddReview.js
+++ src/pages/PlaceDetail/AddReview/AddReview.js
@@ -2,7 +2,28 @@ import React from 'react';
 import './AddReview.css';
 
 function AddReview() {
-  return <div>안녕!</div>;
+  return (
+    <div>
+      <div className="nav_container">
+        {/* 뒤로가기  */}
+        <img
+          className="nav_back"
+          onClick={() => {
+            navigate(-1);
+          }}
+          src="/img/back.png"
+        ></img>
+
+        {/* 타이틀 */}
+        <div>
+          <h2 className="nav_title">내 정보</h2>
+        </div>
+
+        {/* 네비게이션바 */}
+        <img className="nav_btn_img" src="/img/menu.png"></img>
+      </div>
+    </div>
+  );
 }
 
 export default AddReview;
